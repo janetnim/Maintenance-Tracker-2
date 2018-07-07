410eac08b2d444a724c2e9357b6d1464a6f1838c
Janet Wairimu <janetnim4012gmail.com>
2018-07-07 20:08:42 +0300

    [feature]edit buttons for side nav

--- UI/templates/login.js
+++ UI/templates/login.js
@@ -1,3 +1,13 @@
+var btnContainer = document.getElementById("Sidenav");
+var btns = btnContainer.getElementByClassName("lnk");
+for(var i=0; i<btns.length; i++){
+	btns[i]addEventListener("click", function(){
+		var current = document.getElementByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
+}
+
 // user singup method
 function signup(form){
 	fetch('https://maintenance-tracker-2.herokuapp.com/api/v2/auth/signup', {
@@ -552,3 +562,63 @@ fetch("https://maintenance-tracker-2.herokuapp.com/api/v2/users", {
 	}
 	document.getElementById("users").getElementsByTagName("tbody")[0].innerHTML = content;
 })
+
+//pagination
+var current_page = 1;
+var records_per_page = 10;
+var objJson = 
+
+function prevPage(){
+	if(current_page > 1){
+		current_page--;
+		changePage(current_page);
+	}
+}
+
+function nextPage(){
+	if(current_page > numPages){
+		current_page++;
+		changePage(current_page);
+	}
+}
+
+function changePage(page){
+	var btn_next = document.getElementById("btn_next")
+	var btn_prev = document.getElementById("btn_prev");
+    var listing_table = document.getElementById("listingTable");
+    var page_span = document.getElementById("page");
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    listing_table.innerHTML = "";
+
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < objJson.length; i++) {
+        listing_table.innerHTML += objJson[i].adName + "<br>";
+    }
+    page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
+
+function numPages()
+{
+    return Math.ceil(objJson.length / records_per_page);
+}
+
+window.onload = function() {
+    changePage(1);
+};
+
+
