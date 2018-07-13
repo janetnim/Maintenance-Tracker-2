8074e35278d81fe2cc1fdcc07e7b40475d6a5493
Janet Wairimu <janetnim4012gmail.com>
2018-07-13 09:18:12 +0300

    [Refactor] Move css and js files to staic folder

--- UI/templates/logout.js
+++
@@ -1,7 +0,0 @@
-function logout(){
-	var response=confirm("Are you sure you want to log out?");
-	if (response==true){
-		window.location.href="user_login.html"
-	}else{
-	}
-}
--- UI/templates/user.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-	let element = document.getElementById('user')
-	element.style.display = "block"
-	element.innerHTML = getItems().username
-})
